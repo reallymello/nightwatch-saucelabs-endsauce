693634ca5a3e3774b5c6e089c349b21cb961731d
David Mello <theycallmedavidmello@gmail.com>
2022-04-01 16:21:43 -0400

    Fixed bug around undefined sauce region

--- commands/endSauce.js
+++ commands/endSauce.js
@@ -5,22 +5,22 @@ module.exports.command = async function (done) {
         console.info("Test was not run against SauceLabs. Update not required. Exiting endSauce().");
         done();
         return;
-    } else if (!this.options.webdriver.username || !this.options.webdriver.access_key) {
-        console.log("Missing one or more SauceLabs configuration options. Exiting.");
+    } else if (!this.options.username || !this.options.access_key || !this.options.sauce_region) {
+        console.log("Missing one or more SauceLabs configuration options (username, access_key, or sauce_region). Exiting.");
         done();
         return;
     }
 
     const myAccount = new SauceLabs.default({
-        user: this.options.webdriver.username,
-        key: this.options.webdriver.access_key,
-        region: this.options.webdriver.sauce_region
-    });
+        user: this.options.username,
+        key: this.options.access_key,
+        region: this.options.sauce_region
+    })
 
     var sessionid = this.capabilities['webdriver.remote.sessionid'],
         jobName = this.currentTest.name,
         passed = this.currentTest.results.testcases[jobName].failed === 0,
-        groupName = (this.currentTest.group) ? `${this.currentTest.group} - `: "";
+        groupName = (this.currentTest.group) ? `${this.currentTest.group} - ` : "";
 
     console.log(`SauceOnDemandSessionID=${sessionid}\r\n` +
         `job-name==${jobName}\r\n` +
