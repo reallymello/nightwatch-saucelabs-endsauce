2f4b5c2de1f4367e444ce381bcf332a2f8fd5214
David Mello <theycallmedavidmello@gmail.com>
2022-04-01 20:12:13 -0400

    Fixed bug with undefined session id in Nightwatch 2.x

--- commands/endSauce.js
+++ commands/endSauce.js
@@ -1,36 +1,52 @@
-module.exports.command = async function (done) {
-    const SauceLabs = require("saucelabs");
+module.exports = class CustomCommand {
+    async command() {
+        const SauceLabs = require("saucelabs");
 
-    if (this.options.webdriver.host == "localhost") {
-        console.info("Test was not run against SauceLabs. Update not required. Exiting endSauce().");
-        done();
-        return;
-    } else if (!this.options.username || !this.options.access_key || !this.options.sauce_region) {
-        console.log("Missing one or more SauceLabs configuration options (username, access_key, or sauce_region). Exiting.");
-        done();
-        return;
+        let sauceResponse;
+
+        try {
+            if (this.api.options.webdriver.host == "localhost") {
+                console.info("Test was not run against SauceLabs. Update not required. Exiting endSauce().");
+                return sauceResponse;
+            } else if (!this.api.options.username || !this.api.options.access_key || !this.api.options.sauce_region) {
+                console.error("Missing one or more SauceLabs configuration options (username, access_key, or sauce_region). Exiting.");
+                return sauceResponse;
+            }
+
+            const myAccount = new SauceLabs.default({
+                user: this.api.options.username,
+                key: this.api.options.access_key,
+                region: this.api.options.sauce_region
+            })
+
+            var sessionid = this.api.sessionId,
+                jobName = this.api.currentTest.name,
+                passed = this.api.currentTest.results.testcases[jobName].failed === 0,
+                groupName = (this.api.currentTest.group) ? `${this.api.currentTest.group} - ` : "";
+
+            if (!sessionid) {
+                console.log("\r\n❌ Error calling endSauce()")
+                console.error("❌ browser.sessionId undefined. Ensure endSauce() was called before browser was closed. Test result will not be updated.");
+                return sauceResponse;
+            }
+
+            console.log(`SauceOnDemandSessionID=${sessionid}\r\n` +
+                `job-name==${jobName}\r\n` +
+                `passed=${passed}\r\n`);
+
+            sauceResponse = await myAccount.updateJob(process.env.SAUCE_USERNAME,
+                sessionid, {
+                    passed: passed,
+                    name: groupName + jobName
+                });
+        } catch (err) {
+            console.error('Error calling SauceLabs updateJob: ', err);
+            sauceResponse = {
+                status: -1,
+                error: err.message
+            }
+        }
+
+        return sauceResponse;
     }
-
-    const myAccount = new SauceLabs.default({
-        user: this.options.username,
-        key: this.options.access_key,
-        region: this.options.sauce_region
-    })
-
-    var sessionid = this.capabilities['webdriver.remote.sessionid'],
-        jobName = this.currentTest.name,
-        passed = this.currentTest.results.testcases[jobName].failed === 0,
-        groupName = (this.currentTest.group) ? `${this.currentTest.group} - ` : "";
-
-    console.log(`SauceOnDemandSessionID=${sessionid}\r\n` +
-        `job-name==${jobName}\r\n` +
-        `passed=${passed}\r\n`);
-
-    await myAccount.updateJob(process.env.SAUCE_USERNAME,
-        sessionid, {
-            passed: passed,
-            name: groupName + jobName
-        });
-
-    done();
-};
+}
