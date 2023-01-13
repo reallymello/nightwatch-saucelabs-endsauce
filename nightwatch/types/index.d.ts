09e0e5b024b95cca7533c3e446febf4111c722b1
David Mello <theycallmedavidmello@gmail.com>
2023-01-13 08:28:26 -0500

    Fixed type

--- nightwatch/types/index.d.ts
+++ nightwatch/types/index.d.ts
@@ -2,6 +2,13 @@ import { NightwatchAPI, Awaitable } from 'nightwatch';
 
 declare module 'nightwatch' {
   export interface NightwatchCustomCommands {
-    endSauce(): Awaitable<NightwatchAPI>;
+    /**
+     * When called from the afterEach test hook endSauce will update the temporary test run name
+     * and status in the SauceLabs portal with the name of the actual test and the pass or fail
+     * status of the it.
+     * Should be called before closing the browser session.
+     * {@link https://github.com/reallymello/nightwatch-saucelabs-endsauce/blob/master/README.md | Requires configuration}
+     */
+    endSauce(): Awaitable<NightwatchAPI, any>;
   }
 }
