c5b2ba43d1f85ae04a8fb1f6a182a8056c72cbe8
David Mello <theycallmedavidmello@gmail.com>
2023-06-26 17:26:13 -0400

    Merged tests into the same file

--- tests/endToEndTests.js
+++ tests/endToEndTests.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-param-reassign */
+/* eslint-disable no-undef */
+const SauceLabs = require('saucelabs');
 const assert = require('assert');
 
 describe('When called with valid config', () => {
@@ -12,6 +14,31 @@ describe('When called with valid config', () => {
     const result = await browser.endSauce();
     assert.equal(result.status, 'in progress');
     assert.equal(result.passed, true);
+  });
+
+  it('Can use sauce script to update test status and name', async (browser) => {
+    // eslint-disable-next-line new-cap
+    const myAccount = new SauceLabs.default({
+      user: process.env.SAUCE_USERNAME,
+      key: process.env.SAUCE_ACCESS_KEY,
+    });
+
+    await browser
+      .waitForElementVisible('body')
+      .assert.titleContains('David Mello');
+    await browser.endSauceAnnotation();
+
+    const sauceResponse = await myAccount.listJobs(process.env.SAUCE_USERNAME);
+    const jobId = sauceResponse.jobs[0].id;
+    const jobResponse = await myAccount.getJob(
+      process.env.SAUCE_USERNAME,
+      jobId
+    );
+
+    assert.equal(
+      jobResponse.name,
+      'endToEndTests - Can use sauce script to update test status and name'
+    );
   });
 
   after((browser) => {
